2d8a7290a28dd171c8581d6817f5206a9f180253
ykarob <yan@segpay.com>
2020-08-27 14:29:10 -0400

    added refunder, captures, rebills trying to udpate

---
+++ pos/point_of_sale/utils/postpay.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+
+
+postpay_auth_url = 'https://qatrans.segpay.com/PostPay/PostPayAuth/'
+postpay_capt_url = 'https://qatrans.segpay.com/PostPay/PostPayCapture/'
+
+#auth
+amnt= 2.95
+data = {'eticketid'  : '800:100161', 'purchaseid' : 200426873, 'amount' : amnt,  'userdata1': '','userdata2' : '','website': 'qatest'}
+r = requests.post(postpay_auth_url, data)
+str=r.text
+d = json.loads(str)
+transid =d['TransactionId']
+
+
+print (r.text)
+
+#capture
+
+data = {'eticketid'  : '800:100161', 'purchaseid' : 200426873, 'transactionId' : transid, 'amount' : amnt,  'userdata1': '','userdata2' : '','website': 'qatest'}
+r = requests.post(postpay_capt_url, data)
+print (r.text)
+
